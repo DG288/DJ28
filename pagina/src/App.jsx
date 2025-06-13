bd768d703a3df8bcac42c8a57caaa04c121de8e3
dg288 <dg794619@gmail.com>
2025-06-13 20:25:25 +0200

    commit 46

--- pagina/src/App.jsx
+++ pagina/src/App.jsx
@@ -7,18 +7,15 @@ function App() {
   return (
       <div className="container-fluid">
         <div className="contenedor1">
-            <div className="header">
-            </div>
             <div className="section">
-                <fieldset className="text-center">
+                <fieldset className="text-center input-group">
                     <h1>Iniciar Sesión</h1>
-                    <br /><br />
                     <label htmlFor="usuario">Nombre:</label>
                     <input type="text" id="usuario" placeholder="Nombre de usuario" name="usuario" />
-                    <br /><br /><br />
+                    
                     <label htmlFor="Gmail">Gmail:</label>
                     <input type="text" id="Gmail" placeholder="Gmail de usuario" name="Gmail" />
-                    <br /><br /><br />
+                    
                     <label htmlFor="Contraseña">Contraseña:</label>
                     <input type="text" id="Contraseña" placeholder="Contraseña de usuario" name="contraseña" />
                 </fieldset>
